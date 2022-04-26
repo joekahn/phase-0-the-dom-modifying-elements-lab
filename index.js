1d8bebee88e925295e883f5d5553e84be7788b25
Joe Kahn <joekahn19@gmail.com>
2022-04-26 12:59:35 -0700

    commit tree 9cee2ee6cfe1166f964334f31e66d92cdc6a7b2a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Joe is the champion"
+
+document.body.append(newHeader);
+
+
+
